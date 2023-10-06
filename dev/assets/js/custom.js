c3e6977afdee1ba41c3f5a06401058ef0e74843e
pofich <firych23@gmail.com>
2023-10-06 12:22:31 +0300

    fautures fixed

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -16,3 +16,33 @@ button.addEventListener('click', () => {
             currentClassIndex = 0;
           }
 });
+
+
+// var parentElement = document.getElementsByClassName('accordion-item');
+// var childElement = document.getElementsByClassName('accordion-collapse');
+
+// if (childElement.classList.contains('collapse')) {
+//   parentElement.classList.add('newClass');
+// }
+
+
+
+
+var block2 = document.getElementsByClassName('nnn');
+var block1 = document.getElementsByClassName('accordion-collapse');
+
+// Створюємо спостерігача, який відстежуватиме зміни в класах блоку
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    // Перевіряємо, чи доданий клас1 до блоку1
+    if (mutation.target === block1 && mutation.addedNodes.length > 0 && mutation.addedNodes[0].classList.contains('show')) {
+      // Додаємо клас2 до блоку2
+      block2.classList.add('class2');
+    }
+    // Перевіряємо, чи видалений клас1 з блоку1
+    if (mutation.target === block1 && mutation.removedNodes.length > 0 && mutation.removedNodes[0].classList.contains('show')) {
+      // Видаляємо клас2 з блоку2
+      block2.classList.remove('class2');
+    }
+  });
+});
